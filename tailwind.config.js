d5b3e8cb08b82c48762bd0ee472c141729e1b225
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-06-26 10:25:20 +0200

    School section styled, new font -lato.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'white-main': '#ededed',
+        'white-main': '#fefffe',
         'red-main': '#ff2323',
         'black-main': '#111111',
         'blue-tidal': '#0ff',
         'green-spotify': '#1db954',
         'red-apple': '#fc3c44'
+      },
+      fontFamily: {
+        'main': "'Lato', sans-serif",
       }
-    },
   },
   plugins: [],
+  }
 }
-
