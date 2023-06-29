52c0d0096bcefafa3f811301a732276d2b2ab27a
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-06-29 12:41:42 +0200

    Animations: Hamburger, mobile nav.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,13 @@
 const open_menu = document.querySelector('.open-menu')
 const navbar = document.querySelector('.navbar')
+const hamburgerBtn = document.querySelector('.hamburger')
 
 open_menu.addEventListener('click', toggleMenu)
 
 function toggleMenu () {
     navbar.classList.toggle('is-active')
+    hamburgerBtn.classList.toggle('toggle')
 }
+
+
+
